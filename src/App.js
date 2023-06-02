1843215f0cc3519c2737c68cfbcd70099bbe3585
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-02 16:59:57 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Cart from './Components/Cart';
+import Home from './Components/Home';
 import store from './Store';
-import Home from './components/Home';
-import Cart from './components/Cart';
 
 const App = () => {
   return (
